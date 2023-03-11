442eaf218b3bdcfa611e25cfa074746f9fdf66f9
inigo27 <inigoauz.27@gmail.com>
2023-03-11 22:34:02 +0100

    Added Task 1.2 and removed utils functions

--- Week1/main.py
+++ Week1/main.py
@@ -1,16 +1,19 @@
-from utils import readXMLtoAnnotation, annoToDetecFormat
+from utils import readXMLtoAnnotation, annoToDetecFormat, readTXTtoDet
 from metrics import voc_eval, mIoU
 import numpy as np
 
 annotationFile = "../ai_challenge_s03_c010-full_annotation.xml"
 className = "car"
 
+
+# Task 1
+
 annot, imageNames = readXMLtoAnnotation(annotationFile)
 imageIds, BB = annoToDetecFormat(annot, className)
 
 
 # No confidence values, repeat N times with random values
-N = 10
+N = 1#10
 apSum = 0
 for i in range(N):
     conf = np.random.rand(len(imageIds))
@@ -20,3 +23,24 @@ print("mAP: ", apSum/N)
 
 miou = mIoU((imageIds, conf, BB), annot, imageNames, className)
 print("mIoU: ", miou)
+
+# Need to add noise
+
+# Task 1.2
+
+detFolder = "../AICity_data/train/S03/c010/det/"
+detMaskRcnn = "det_mask_rcnn.txt"
+detSSD = "det_ssd512.txt"
+detYolo3 = "det_yolo3.txt"
+
+imageIds, conf, BB = readTXTtoDet(detFolder + detMaskRcnn)
+_,_, ap = voc_eval((imageIds, conf, BB), annot, imageNames, "car")
+print("Mask RCNN mAP: ", ap)
+imageIds, conf, BB = readTXTtoDet(detFolder + detSSD)
+_,_, ap = voc_eval((imageIds, conf, BB), annot, imageNames, "car")
+print("SSD512 mAP: ", ap)
+imageIds, conf, BB = readTXTtoDet(detFolder + detYolo3)
+_,_, ap = voc_eval((imageIds, conf, BB), annot, imageNames, "car")
+print("Yolo3 mAP: ", ap)
+
+# Task 2
--- Week1/utils.py
+++ Week1/utils.py
@@ -3,6 +3,108 @@ import cv2
 import numpy as np
 import itertools
 from collections import OrderedDict
+import copy
+import xml.etree.ElementTree as ET
+import numpy as np
+import cv2
+
+# Read the XML file of week 1 GT annotations
+def readXMLtoAnnotation(annotationFile):
+    # Read XML
+    file = ET.parse(annotationFile)
+    root = file.getroot()
+    
+    annotations = {}
+    image_ids = []
+    # Find objects
+    for child in root:
+        if child.tag == "track":
+            # Get class
+            className = child.attrib["label"]
+            for obj in child:
+                frame = obj.attrib["frame"]
+                xtl = float(obj.attrib["xtl"])
+                ytl = float(obj.attrib["ytl"])
+                xbr = float(obj.attrib["xbr"])
+                ybr = float(obj.attrib["ybr"])
+                bbox = [xtl, ytl, xbr, ybr]
+                if frame in image_ids:
+                    annotations[frame].append({"name": className, "bbox": bbox})
+                else:
+                    image_ids.append(frame)
+                    annotations[frame] = [{"name": className, "bbox": bbox}]
+    
+    
+    return annotations, image_ids
+
+# Read txt detection lines to annot
+def readTXTtoDet(txtPath):
+    # Read file
+    file = open(txtPath, 'r')
+    lines = file.readlines()
+    # Init values
+    imageIds = []
+    confs = []
+    BB = np.zeros((0,4))
+    # Insert every detection
+    for line in lines:
+        #frame,-1,left,top,width,height,conf,-1,-1,-1
+        splitLine = line.split(",")
+        # Frame
+        imageIds.append(str(int(splitLine[0])-1))
+        # Conf
+        confs.append(float(splitLine[6]))
+        # BBox
+        left = float(splitLine[2])
+        top = float(splitLine[3])
+        width = float(splitLine[4])
+        height = float(splitLine[5])
+        xtl = left
+        ytl = top
+        xbr = left + width - 1
+        ybr = top + height - 1
+        BB = np.vstack((BB, np.array([xtl,ytl,xbr,ybr])))
+    
+    file.close()
+    
+    return (imageIds, np.array(confs), BB)
+
+
+# Parse from annotations format to detection format
+def annoToDetecFormat(annot, className):
+    
+    imageIds = []
+    BB = np.zeros((0,4))
+    
+    for imageId in annot.keys():
+        for obj in annot[imageId]:
+            
+            if obj["name"] == className:
+                imageIds.append(imageId)
+                BB = np.vstack((BB, obj["bbox"]))
+    
+    return imageIds, BB
+
+# Draw detection and annotation boxes in image
+def drawBoxes(img, det, annot, colorDet, colorAnnot, className):
+    img = img.copy()
+    
+    # Draw annotations
+    for obj in annot:
+        if obj["name"] == className:
+            # Draw box
+            bbox = obj["bbox"]
+            img = cv2.rectange(img, (int(bbox[0]), int(bbox[1])), 
+                               (int(bbox[2]), int(bbox[3])), colorAnnot, 3)
+    
+    # Draw detections
+    for i in det.shape[0]:
+        # Draw box
+        bbox = det[i,:]
+        img = cv2.rectange(img, (int(bbox[0]), int(bbox[1])), 
+                           (int(bbox[2]), int(bbox[3])), colorDet, 3)
+    
+    return img
 
 def reader(nombre_archivo):
     # Inicializamos la lista de vectores
@@ -30,6 +132,7 @@ def group(boxes):
     grouped = itertools.groupby(boxes_sorted, key=lambda box: box.frame)
     # Creamos un OrderedDict con las listas de boxes agrupados
     return OrderedDict((frame, list(boxes)) for frame, boxes in grouped)
+
 class BBox:
     def __init__(self, data):
         self.frame = int(data[0])
