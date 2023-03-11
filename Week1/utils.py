65575a94fc8bf4e0f3ca77682dbe14793ad04c49
Michells13 <114994875+Michells13@users.noreply.github.com>
2023-03-11 12:43:20 +0100

    Update utils.py

--- Week1/utils.py
+++ Week1/utils.py
@@ -30,4 +30,62 @@ def group(boxes):
     grouped = itertools.groupby(boxes_sorted, key=lambda box: box.frame)
     # Creamos un OrderedDict con las listas de boxes agrupados
     return OrderedDict((frame, list(boxes)) for frame, boxes in grouped)
+class BBox:
+    def __init__(self, data):
+        self.frame = int(data[0])
+        self.id = int(data[1])
+        self.left = int(data[2])
+        self.top = int(data[3])
+        self.width = int(data[4])
+        self.height = int(data[5])
+        self.conf = float(data[6])
+        self.x_center = float(data[7])
+        self.y_center = float(data[8])
+        self.z_center = float(data[9])
+    
+    @property
+    def area(self):
+        return self.width * self.height
+    
+    @property
+    def bottom(self):
+        return self.top + self.height
+    
+    @property
+    def right(self):
+        return self.left + self.width
+    
+    @property
+    def top_left(self):
+        return (self.left, self.top)
+    
+    @property
+    def top_right(self):
+        return (self.right, self.top)
+    
+    @property
+    def bottom_left(self):
+        return (self.left, self.bottom)
+    
+    @property
+    def bottom_right(self):
+        return (self.right, self.bottom)
+    
+    @property
+    def x_min(self):
+        return self.left
+    
+    @property
+    def x_max(self):
+        return self.right
+    
+    @property
+    def y_min(self):
+        return self.top
+    
+    @property
+    def y_max(self):
+        return self.bottom
+
+
 
