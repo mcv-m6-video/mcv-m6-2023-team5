39f3307bc42a217f4120e1c9c86e3d25ff4ec213
inigo27 <inigoauz.27@gmail.com>
2023-03-14 12:30:22 +0100

    Change task2 to reduce num images

--- Week1/task2.py
+++ Week1/task2.py
@@ -2,7 +2,7 @@ import cv2
 import matplotlib
 matplotlib.use('agg')
 import imageio
-from utils import readXMLtoAnnotation, readTXTtoDet, drawBoxes
+from utils import readXMLtoAnnotation, readTXTtoDet, drawBoxes, annoToDetecFormat
 from metrics import mIoU
 from noise import add_noise
 import numpy as np
@@ -30,16 +30,21 @@ annot, imageNames = readXMLtoAnnotation(annotationFile)
     
 noiseMean = 5.0
 noiseStd = 50.0
-probRem = 0.0
-probGen = 0.0
+probRem = 0.2
+probGen = 0.1
     
 # Get noisy annotations
 annot_noise = copy.deepcopy(annot)
 annot_noise = add_noise(annot_noise, imageNames, noiseMean, noiseStd, probRem, probGen)
     
-detModel = "mask_rcnn"
-detValues = detMaskRcnn
-det = readTXTtoDet(detFolder + detValues)  
+detModel = "noise"
+detValues = detYolo3
+if detModel == "noise":
+    imageIds, BB = annoToDetecFormat(annot_noise, className)
+    conf = np.random.rand(len(imageIds))
+    det = (imageIds, conf, BB)
+else:
+    det = readTXTtoDet(detFolder + detValues)  
 
 # open the video file
 cap = cv2.VideoCapture("../AICity_data/train/S03/c010/vdo.avi")
@@ -57,36 +62,38 @@ while True:
     if not ret:
         break
     frame_num = int(cap.get(cv2.CAP_PROP_POS_FRAMES) - 1)
-    # Get frame
-    annotFrame = {str(frame_num): annot[str(frame_num)]}
-    frameIndex = np.array(det[0]) == str(frame_num)
-    detFrame = (np.array(det[0])[frameIndex], det[1][frameIndex], det[2][frameIndex,:])
-    # Draw box
-    frameDraw = drawBoxes(frame, detFrame[2], annotFrame[str(frame_num)], red, blue, className)
-    frameDraw = cv2.resize(frameDraw, (500,250))
-    
-    
-    # Compute mIoU
-    miou = mIoU(detFrame, annotFrame, [str(frame_num)], className)
-    iou_gif.append(miou)
-    frame_idx.append(frame_num)
-    
-    fig = Figure()
-    canvas = FigureCanvas(fig)
-    ax = fig.gca()
-    ax.set(xlim=(0, int(det[0][-1])))
-    ax.set_xlabel("Frame")
-    ax.set_ylabel("mIoU")
-    ax.plot(frame_idx, iou_gif, linewidth=1)
-    
-    canvas.draw()       # draw the canvas, cache the renderer
-    image = np.frombuffer(canvas.tostring_rgb(), dtype='uint8')
-    image = image.reshape(canvas.get_width_height()[::-1] + (3,))
-    image = cv2.resize(image, (500,250))
-    
-    # Concatenate two images
-    finalImage = np.vstack((frameDraw, image))
-    frames_gif.append(finalImage)
+    # Reduce number of images
+    if frame_num % 10 == 0:
+        # Get frame
+        annotFrame = {str(frame_num): annot[str(frame_num)]}
+        frameIndex = np.array(det[0]) == str(frame_num)
+        detFrame = (np.array(det[0])[frameIndex], det[1][frameIndex], det[2][frameIndex,:])
+        # Draw box
+        frameDraw = drawBoxes(frame, detFrame[2], annotFrame[str(frame_num)], red, blue, className)
+        frameDraw = cv2.resize(frameDraw, (500,250))
+        
+        
+        # Compute mIoU
+        miou = mIoU(detFrame, annotFrame, [str(frame_num)], className)
+        iou_gif.append(miou)
+        frame_idx.append(frame_num)
+        
+        fig = Figure()
+        canvas = FigureCanvas(fig)
+        ax = fig.gca()
+        ax.set(xlim=(0, int(det[0][-1])))
+        ax.set_xlabel("Frame")
+        ax.set_ylabel("mIoU")
+        ax.plot(frame_idx, iou_gif, linewidth=0.5)
+        
+        canvas.draw()       # draw the canvas, cache the renderer
+        image = np.frombuffer(canvas.tostring_rgb(), dtype='uint8')
+        image = image.reshape(canvas.get_width_height()[::-1] + (3,))
+        image = cv2.resize(image, (500,250))
+        
+        # Concatenate two images
+        finalImage = np.vstack((frameDraw, image))
+        frames_gif.append(finalImage)
     
 
 # Save the video frames as a GIF
